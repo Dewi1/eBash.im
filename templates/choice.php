6a70631064a3dd6ab672a2681ea46374617ceebe
Dewi1 <roack@yandex.ru>
2014-12-30 17:52:33 +0200

    all

--- templates/choice.php
+++ templates/choice.php
@@ -1,39 +1,45 @@
 <?php $title = 'choice'; ?>
 
-<form  method="post" action="/index.php?page=choice">
-    <center>
-        <p>Введите страницы для скачивания:</p>
-        <p>C: <input name="first" type="number" max="1500" min="1" value="1" style="width:100px" title="1-1500">
-        По: <input name="second" type="number" max="1500" min="1" value="1" style="width:100px" title="1-1500"></p>
-    </center>
-    <center><br>
-        <form action="bash.php" method="post">
-            <input type="submit" value="Сохранить" name="save">
-        </form>
-    </center>
-</form>
-<?php if($_POST['save'] == "Сохранить"):?>
-    <center><br>
-    <? $arr_max = max_page();?>
-    <?php $l = $_POST['first']; $k = $_POST['second']; ?>
-    <?php $text = save_all($l, $k);?>
-    <?php if ($k >= $l):?>
-        В папку "saves" были сохранены файлы:<br><br>
-        <?php for($n=$l; $n<=$k; $n++):?>
-            <?php if  ($n <= $arr_max[1]):?>
-                <?php echo 'Bash_'.$n.'.txt';?><br>
-            <?php else:?>
-                Файл: <?php echo 'Bash_'.$n.'.txt';?> не был сохранен.<br>
-            <?php endif?>
-        <?php endfor ?>
-    <?php else:?>
-        Файлы не были сохранены.
-    <?php endif?><br>
-    <?php if  ($n > $arr_max[1]):?>
-        Таких страниц не существует.
+<?php if($_SESSION['auth']=='admin' || $_SESSION['auth']=='user'):?>
+    <form  method="post" action="/index.php?page=choice">
+        <center>
+            <p>Введите страницы для скачивания:</p>
+            <p>C: <input name="first" type="number" max="1500" min="1" value="1" style="width:100px" title="1-1500">
+            По: <input name="second" type="number" max="1500" min="1" value="1" style="width:100px" title="1-1500"></p>
+        </center>
+        <center><br>
+            <form action="bash.php" method="post">
+                <input type="submit" value="Сохранить" name="save">
+            </form>
+        </center>
+    </form>
+    <?php if($_POST['save'] == "Сохранить"):?>
+        <center><br>
+        <? $arr_max = max_page();?>
+        <?php $l = $_POST['first']; $k = $_POST['second']; ?>
+        <?php $text = save_all($l, $k);?>
+        <?php if ($k >= $l):?>
+            В папку "saves" были сохранены файлы:<br><br>
+            <?php for($n=$l; $n<=$k; $n++):?>
+                <?php if  ($n <= $arr_max[1]):?>
+                    <?php echo 'Bash_'.$n.'.txt';?><br>
+                <?php else:?>
+                    Файл: <?php echo 'Bash_'.$n.'.txt';?> не был сохранен.<br>
+                <?php endif?>
+            <?php endfor ?>
+        <?php else:?>
+            Файлы не были сохранены.
+        <?php endif?><br>
+        <?php if  ($n > $arr_max[1]):?>
+            Таких страниц не существует.
+        <?php endif?>
+        </center>
     <?php endif?>
+<?php else: ?>
+    <center><br>
+        <h3>Сперва вы должны авторизироваться на сайте!</h3>
     </center>
-<?php endif?>
+<?php endif ?>
 <center><br><br><br>
     <div style=" width: 800px; align=center">
         <button type="button">
--- templates/save.php
+++ templates/save.php
@@ -1,15 +1,21 @@
 <?php $title = 'Save'; ?>
 <form  method="post" action="/index.php?page=save">
-    <center><br>
-        <div style=" width: 800px; align=center">
-            <input type="submit" value="Сохранить главную страницу" name="save2">
-        </div>
-        <br><h3>Документ будет сохранен как "Bash.txt" через 5 секунд после нажатия кнопки.</h3>
-    </center><br>
-    <?php if($_POST['save2'] == "Сохранить главную страницу"):?>
-        <?php $current = file_save($arr_text);?>
-        <?php sleep(5); ?>
-        <?php $file = file_download($file)?>
+    <?php if($_SESSION['auth']=='admin' || $_SESSION['auth']=='user'):?>
+        <center><br>
+            <div style=" width: 800px; align=center">
+                <input type="submit" value="Сохранить главную страницу" name="save2">
+            </div>
+            <br><h3>Документ будет сохранен как "Bash.txt" через 5 секунд после нажатия кнопки.</h3>
+        </center><br>
+        <?php if($_POST['save2'] == "Сохранить главную страницу"):?>
+            <?php $current = file_save($arr_text);?>
+            <?php sleep(5); ?>
+            <?php $file = file_download($file)?>
+        <?php endif ?>
+    <?php else: ?>
+        <center><br>
+            <h3>Сперва вы должны авторизироваться на сайте!</h3>
+        </center>
     <?php endif ?>
 </form><br>
 <center><br>
