c581908ab4dae4af2eceb49dabd345fea721acec
Dewi1 <roack@yandex.ru>
2015-01-31 12:14:23 +0200

    plugin

--- tasks/File_size.php
+++ tasks/File_size.php
@@ -12,36 +12,27 @@ Author: Dewi1
 Version: 1.0
 Author URI: https://test.ru/wordpress/
 */
-function pict_info() {
+function picture_take_info() {
     if(upload_gallery_image()) {//str. 1566 in "gallery-plugin.php"
         if(save()){
             $text = '';
             $name = getName();
             $size = getSize();
             list($width, $height, $type, $attr) = getimagesize($name);
-            $string = write_string($name, $size, $type, $width, $height);
-            file_put_contents($text, $string);
-            file_download($text);
+            save_xml_info($name, $size, $type, $width, $height);
         }
     }
 }
-function write_string($name, $size, $type, $width, $height, $string = 0){
-    $string .= 'Name: '.$name.'Type: '.$type.'Size: '.$size.'Width: '.$width.'Height: '.$height;
-    return $string;
-}
-function file_download($text) {
-    if (ob_get_level()) {
-        ob_end_clean();
-    }
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="Pictures_info.txt"');
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . strlen($text));
-    echo $text;
-
-    return $text;
+function save_xml_info($name, $type, $width, $height) {
+    $dom_xml= new DomDocument();
+    $dom_xml->loadXML(
+        '<picture>
+            <name>'.$name.'</name>
+            <type>'.$type.'</type>
+            <width>'.$width.'</width>
+            <height>'.$height.'</height>
+        </picture>'
+    );
+    $path="Gallery.xml";
+    echo $dom_xml->save($path);
 }
